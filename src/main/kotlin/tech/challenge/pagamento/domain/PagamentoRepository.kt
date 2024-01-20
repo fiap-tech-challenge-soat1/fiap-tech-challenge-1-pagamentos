ec947b19b2b39a1fe5d2f1bad3e9b41bdec56d89
Paulo Squisatti <paulomontanger@gmail.com>
2024-01-20 19:07:56 -0300

    Migrado do mongodb para o firestore

--- src/main/kotlin/tech/challenge/pagamento/domain/PagamentoRepository.kt
+++ src/main/kotlin/tech/challenge/pagamento/domain/PagamentoRepository.kt
@@ -1,44 +1,48 @@
 package tech.challenge.pagamento.domain
 
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.mongodb.repository.Query
+import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 @Repository
-interface PagamentoRepository: MongoRepository<Pagamento, String> {
+interface PagamentoRepository: FirestoreReactiveRepository<Pagamento> {
 
-    @Query(
-        """
-            {
-                pedidoId: ?0,
-                status: { 
-                    "${'$'}in" : [
-                      "SUCESSO",
-                      "PENDENTE"
-                    ]
-                }
-            }
-        """,
-        exists = true
-    )
-    fun existePagamentoRealizadoOuPendenteParaPedidoId(pedidoId: Long): Boolean
+//    @Query(
+//        """
+//            {
+//                pedidoId: ?0,
+//                status: {
+//                    "${'$'}in" : [
+//                      "SUCESSO",
+//                      "PENDENTE"
+//                    ]
+//                }
+//            }
+//        """,
+//        exists = true
+//    )
+//    fun existePagamentoRealizadoOuPendenteParaPedidoId(pedidoId: Long): Boolean
+//
+//    @Query(
+//        """
+//            {
+//                pedidoId: ?0,
+//                status: "PENDENTE"
+//            }
+//        """,
+//    )
+//    fun encontrarPagamentoPendenteParaPedidoId(pedidoId: Long): Pagamento?
 
-    @Query(
-        """
-            { 
-                pedidoId: ?0,
-                status: "PENDENTE"
-            }
-        """,
-    )
-    fun encontrarPagamentoPendenteParaPedidoId(pedidoId: Long): Pagamento?
+//    @Query(
+//        """
+//            {
+//                pedidoId: ?0
+//            }
+//        """
+//    )
+//    fun encontrarPagamentoParaPedidoId(pedidoId: Long): Pagamento?
 
-    @Query(
-        """
-            {
-                pedidoId: ?0
-            }
-        """
-    )
-    fun encontrarPagamentoParaPedidoId(pedidoId: Long): Pagamento?
+//    fun existsByPedidoIdAndStatusIn(pedido: Long, status: List<PagamentoStatus>): Mono<Boolean>
+
+    fun findByPedidoId(pedidoId: Long): Mono<Pagamento?>
 }
--- src/main/kotlin/tech/challenge/pagamento/domain/PagamentoService.kt
+++ src/main/kotlin/tech/challenge/pagamento/domain/PagamentoService.kt
@@ -1,5 +1,6 @@
 package tech.challenge.pagamento.domain
 
+import org.apache.commons.lang3.NotImplementedException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import tech.challenge.pagamento.domain.exception.BusinessException
@@ -12,25 +13,29 @@ class PagamentoService {
     lateinit var pagamentoRepository: PagamentoRepository
 
     fun processarPagamento(novoPagamentoRequestDto: NovoPagamentoRequestDto): PagamentoDto {
-        if(pagamentoRepository.existePagamentoRealizadoOuPendenteParaPedidoId(novoPagamentoRequestDto.pedido)) {
+        pagamentoRepository.findByPedidoId(novoPagamentoRequestDto.pedido).block()?.run {
             throw BusinessException("Pedido já possui pagamento realizado ou em processamento")
         }
 
         return Pagamento.createFrom(novoPagamentoRequestDto).also {
-            pagamentoRepository.save(it)
+            pagamentoRepository.save(it).block()
         }.toPagamentoDto()
     }
 
     fun confirmarPagamento(pedido: Long, status: PagamentoStatus): PagamentoDto {
-        return pagamentoRepository.encontrarPagamentoPendenteParaPedidoId(pedido)?.let {
-            it.status = status
-            pagamentoRepository.save(it).toPagamentoDto()
-        } ?: throw NotFoundException("Não foi encontrado pagamento pendente para o pedido")
+        val pagamento  = pagamentoRepository.findByPedidoId(pedido).block() ?: throw NotFoundException("Não foi encontrado pagamento pendente para o pedido")
+
+        if(pagamento.status != PagamentoStatus.PENDENTE) {
+            throw BusinessException("O pagamento desse pedido não esta mais pendente")
+        }
+
+        pagamento.status = status
+        return pagamentoRepository.save(pagamento).block()!!.toPagamentoDto()
 
         //TODO notificar serviço de pedido
     }
 
     fun consultarStatusPagamento(pedido: Long): PagamentoDto {
-        return pagamentoRepository.encontrarPagamentoParaPedidoId(pedido)?.toPagamentoDto() ?: throw NotFoundException("Não foi encontrado pagamento para o pedido")
+        return pagamentoRepository.findByPedidoId(pedido).block()?.toPagamentoDto() ?: throw NotFoundException("Não foi encontrado pagamento para o pedido")
     }
 }
