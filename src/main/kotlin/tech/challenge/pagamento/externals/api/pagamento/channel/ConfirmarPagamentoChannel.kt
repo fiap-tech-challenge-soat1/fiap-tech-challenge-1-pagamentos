2fbe19294e6d28c2f389c73c0ac619e135480bd1
Paulo Squisatti <paulomontanger@gmail.com>
2024-03-19 18:10:28 -0300

    Ajuste logs

--- src/main/kotlin/tech/challenge/pagamento/externals/api/pagamento/channel/ConfirmarPagamentoChannel.kt
+++ src/main/kotlin/tech/challenge/pagamento/externals/api/pagamento/channel/ConfirmarPagamentoChannel.kt
@@ -1,19 +1,24 @@
 package tech.challenge.pagamento.externals.api.pagamento.channel
 
+import com.google.gson.Gson
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.stereotype.Component
 import tech.challenge.pagamento.domain.pagamento.dto.ResultadoPagamentoDto
+import java.util.logging.Logger
 
 const val BINDING_NAME_CONFIRMAR_PAGAMENTO_TOPIC = "confirmarPagamentoSupplier"
 
 @Component
 class ConfirmarPagamentoChannel: IConfirmarPagamentoChannel {
 
+    val logger: Logger = Logger.getLogger(ConfirmarPagamentoChannel::class.simpleName)
+
     @Autowired
     lateinit var streamBridge: StreamBridge
 
     override fun confirmarPagamento(resultadoPagamentoDto: ResultadoPagamentoDto) {
         streamBridge.send(BINDING_NAME_CONFIRMAR_PAGAMENTO_TOPIC, resultadoPagamentoDto)
+        logger.info("[SERVICO-PAGAMENTO][TOPIC] - Mensagem enviada no topico confirmar-pagamento-topic ${Gson().toJson(resultadoPagamentoDto)}")
     }
 }
